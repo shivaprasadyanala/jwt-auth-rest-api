d17b18d3e0423797777c5d50f626ae0797d27923
shivaprasadyanala <shivaprasadyanala@gmail.com>
2022-03-26 08:35:33 +0530

    initial

---
+++ auth.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const router = require('express').Router();
+
+var bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const User = require('./models/userModel');
+const { response } = require('express');
+//user signup
+router.post("/signup", async(req, res, next) => {
+        //generate new password
+        const salt = await bcrypt.genSalt(8);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        //create new user
+
+        const newUser = new User({
+            email: req.body.email,
+            name: req.body.name,
+            password: hashedPassword
+        })
+
+        try {
+
+            //save user and respond
+            const user = await newUser.save();
+            // res.status(200).json(user);
+            // return
+        } catch (err) {
+            const error = new Error("Error! Something went wrong.");
+            return next(error);
+        }
+        // var token = jwt.sign({
+        //         userId: newUser.id,
+        //         email: newUser.email
+        //     }, "secretkeyappearshere", { expiresIn: 86400 })
+        let token;
+        try {
+            token = jwt.sign({ userId: newUser.id, email: newUser.email },
+                "secretkeyappearshere", { expiresIn: "1h" }
+            )
+        } catch (err) {
+            const error = new Error("Error! something went wrong");
+            return next(error);
+        }
+        res
+            .status(201)
+            .json({
+                success: true,
+                data: { userId: newUser.id, email: newUser.email, token: token },
+                message: "signup succesful",
+                accessToken: token,
+            });
+    })
+    //user login
+router.post('/login', async(req, res) => {
+    let existinguser;
+    try {
+        existinguser = await User.findOne({ email: req.body.email });
+        !existinguser && res.status(400).json("user not found");
+        const validPassword = await bcrypt.compare(req.body.password, existinguser.password);
+        !validPassword && res.status(400).json("invalid password");
+    } catch (err) {
+        const error = Error("Wrong details please check at once");
+        return next(error);
+    }
+    let token;
+    try {
+        token = jwt.sign({ userId: existinguser.id, email: existinguser.email },
+            "secretkeyappearshere", { expiresIn: "1h" }
+        );
+    } catch (err) {
+        console.log(err);
+        const error = new Error("Error! Something went wrong.");
+        return next(error);
+    }
+    res
+        .status(201)
+        .json({
+            success: true,
+            data: {
+                userId: existinguser.id,
+                email: existinguser.email,
+                token: token,
+            },
+            message: "login succesful",
+            accessToken: token,
+        });
+})
+
+router.get('/accessResource', (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+        response.status(200).json({ success: false, message: "Error! Token not provided" });
+    }
+    //decodering the token
+    const decodedToken = jwt.verify(token, "secretkeyappearshere");
+    res.status(200).json({ success: true, data: { userId: decodedToken.user, email: decodedToken.email } })
+})
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,73 @@
+PORT = 3000;
+var express = require('express');
+var bodyParser = require('body-parser');
+
+const mongoose = require('mongoose')
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//cors
+const cors = require('cors')
+app.use(cors())
+
+const User = require('./models/userModel.js')
+const Product = require('./models/productModel.js')
+const auth = require('./auth')
+
+//get all the products
+app.get('/user', async(req, res) => {
+        const products = await Product.find();
+        res.send(products)
+    })
+    //get the product by user using id
+app.get('/user/:id', async(req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.send(product)
+})
+
+//adding products by admin
+app.post('/admin/addproducts', async(req, res) => {
+        var newProduct = new Product(req.body);
+        try {
+            const savedProduct = await newProduct.save();
+            res.status(200).json(savedProduct);
+        } catch (err) {
+            res.status(500).json(err)
+        }
+        // res.json({ message: "New product created." });
+    })
+    //updating the product by admin
+app.put('/admin/:id', async(req, res) => {
+    var id = req.params.id;
+    var product = Product.findById(id);
+    if (product) {
+        await product.updateOne(req.body);
+        res.status(200).json("the post has been updated");
+    } else {
+        res.status(403).json("error while updating");
+    }
+    //delete a product using the id by admin
+    app.delete('/admin/:id', async(req, res) => {
+        const { id } = req.params;
+        var product = Product.findById(id);
+        if (product) {
+            await product.deleteOne();
+            res.status(200).json("the post has been updated");
+        } else {
+            res.status(403).json("error while deleting");
+        }
+    })
+})
+app.use("/auth", auth);
+mongoose
+    .connect("mongodb://localhost:27017/testDB")
+    .then(() => {
+        app.listen("3000", () => {
+            console.log("Server is listening on port 3000");
+        });
+    })
+    .catch((err) => {
+        console.log("Error Occured");
+    });
